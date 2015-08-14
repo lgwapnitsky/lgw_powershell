f70b5be98e3b93cced1c58de78e5dbe9a49a422b
Larry G. Wapnitsky <larry@wapnitskycom>
2015-08-14 16:20:41 -0400

    more prototyping on proxyaddresses modification

--- testing/proxyaddress_export.ps1
+++ testing/proxyaddress_export.ps1
@@ -1,22 +1,67 @@
-$userinfo = foreach ($user in get-qaduser "larry.wapnitsky" | where {
+#$userinfo =
+foreach ($user in get-qaduser larry.wapnitsky  | where {
     ($_.accountisdisabled -eq $false) -and
     ($_.proxyaddresses -ne $null) })
-{
-  New-object -type PSObject -property @{
-  username = $user.samaccountname
-  name = $user.name
-  proxyAddresses = (@($user.proxyAddresses) -join ',')
+  {
+#  $x =  New-object -type PSObject -property @{
+    $username = $user.samaccountname
+    $name = $user.name
+    $dottedname = $name.replace(' ','.')
+    $proxyAddresses = (@($user.proxyAddresses) -join ',')
+    [System.Collections.ArrayList]$pas = $proxyAddresses -split ','
+    $mngMail = $dottedname + "@MNGDirect.com"
+#  }
+
+  $mngExists = $false
+
+  foreach ($line in $pas) {
+    $rx  = [regex]::matches($line,"((?i)SMTP)\:(.*)((?i)\@mngdirect\.com)")
+    if ($rx.success -eq $true)
+    {
+      switch ($rx.groups[0] -match $dottedname)
+      {
+        true
+        {
+          switch ($rx.groups[0] -match [regex]"SMTP")
+          {
+            false
+            {
+              $repline = [regex]::Replace($line,"((?-i)smtp)", "SMTP")
+              $mngExists = $True
+            }
+          }
+        }
+        default
+        {
+          $repline = [regex]::Replace($line,"((?-i)SMTP)","smtp")
+        }
+      }
+    }
+    else {    $repline = [regex]::Replace($line,"((?-i)SMTP)","smtp") }
+
+    $pas = $pas.replace($line, $repline)
   }
-}#
 
-$userinfo | export-csv -NoTypeInformation "c:\users\larry.wapnitsky\documents\metapharm\active_proxyaddresses_20150814.csv"
+  $pas.sort()
 
 
-[System.Collections.ArrayList]$pas = $userinfo[0].proxyAddresses -split ','
-foreach ($line in $pas){
-  $repline = [regex]::Replace($line,"((?-i)SMTP)","smtp")
-  $pas = $pas.replace($line, $repline)
+  if (!($mngExists))
+    #if (!(($pas.BinarySearch("smtp:"+$mngmail)) -or ($pas.BinarySearch("SMTP:"+$mngMail))))
+  {
+    $pas.add("SMTP:" + $mngMail) | out-null
+    $newpas = $pas -join ','
+    [string]$pas = $newpas
+
+#    set-qaduser $user -ObjectAttributes @{ProxyAddresses = $newpas} | out-null
+
+    #$pas
+  }
+#  else {write-host "exists";}
+
+  set-qaduser $user -ObjectAttributes @{ProxyAddresses = $newpas} | out-null
 }
 
-$newpas = $pas -join ','
-[string]$pas = $newpas
+#
+
+#$userinfo | export-csv -NoTypeInformation "c:\users\larry.wapnitsky\documents\metapharm\active_proxyaddresses_20150814.csv"
+#$userinfo
